05c93dc13851494f51e60af2b9efb0bf7ca8be4d
shekhus <cpsatav@gmail.com>
2022-11-10 03:35:34 +0530

    add truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,2 +1,18 @@
-module.exports = {
-}
+//https://goerli.infura.io/v3/12d81d2652574509b991c7f01ffbd77f
+const fs =require('fs');
+const HDWalletProvider = require('truffle-hdwallet-provier');
+
+const secrets = JSON.parse(fs.readFileSync(',secrets').toString().trim());
+// truffle-hdwallet-provier
+module.exports = {networks : {
+    goerli:{
+        provider:() =>
+            new HDWalletProvider(
+                secrets.seed,
+                `https://goerli.infura.io/v3/${secrets.projectId}`
+
+            ),
+            network_id :5
+            }
+        }
+    }
