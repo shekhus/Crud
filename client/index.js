b17375b5075bbe8b76cd92eb03af6540eb4f7f83
shekhus <cpsatav@gmail.com>
2022-11-10 22:20:59 +0530

    frontend complete

--- client/index.js
+++ client/index.js
@@ -6,8 +6,10 @@ let crud;
 
 const initWeb3 = () => {
   return new Promise((resolve, reject) => {
+     //case 1- New Metamask is present 
     if(typeof window.ethereum !== 'undefined') {
       const web3 = new Web3(window.ethereum);
+       //ask user to connect Metamask 
       window.ethereum.enable()
         .then(() => {
           resolve(
@@ -19,17 +21,22 @@ const initWeb3 = () => {
         });
       return;
     }
+     //case 2-Old Metamask is present 
     if(typeof window.web3 !== 'undefined') {
       return resolve(
         new Web3(window.web3.currentProvider)
       );
     }
+    //case 3- no metamasak.use Ganache 
     resolve(new Web3('http://localhost:9545'));
   });
 };
 
+//Pointer to deploy instance of the contract to interact
+//import the Crud object from Abi and address 
 const initContract = () => {
   const deploymentKey = Object.keys(Crud.networks)[0];
+    // instiantiate the web3 contract instance 
   return new web3.eth.Contract(
     Crud.abi, 
     Crud
@@ -38,15 +45,25 @@ const initContract = () => {
   );
 };
 
+//Business logic for the code 
 const initApp = () => {
+  //Two pointers for create function 
   const $create = document.getElementById('create');
   const $createResult = document.getElementById('create-result');
+
+    //Two pointers for read function 
   const $read = document.getElementById('read');
   const $readResult = document.getElementById('read-result');
+
+  //Two Pointers for update function 
   const $edit = document.getElementById('edit');
   const $editResult = document.getElementById('edit-result');
+
+   //Two Pointers for Destroy function 
   const $delete = document.getElementById('delete');
   const $deleteResult = document.getElementById('delete-result');
+
+  //In order to javascript code intearact with accounts from metamask 
   let accounts = [];
 
   web3.eth.getAccounts()
@@ -54,18 +71,27 @@ const initApp = () => {
     accounts = _accounts;
   });
 
+  //add Event Litener to create form and wait for event submitted
   $create.addEventListener('submit', (e) => {
+      //to stop default reloading 
     e.preventDefault();
+    //extract the value of name input
     const name = e.target.elements[0].value;
-    crud.methods.create(name).send({from: accounts[0]})
+    //trigger the create method on smartcontract 
+    crud.methods
+    .create(name)
+    .send({from: accounts[0]})
+
+    //we wait for completion of this promise 
     .then(result => {
-      $createResult.innerHTML = `New user ${name} successfully created`;
+      $createResult.innerHTML = `New user ${name} successfully created !`;
     })
     .catch(_e => {
       $createResult.innerHTML = `Ooops... there was an error while trying to create a new user...`;
     });
   });
 
+  //Read function
   $read.addEventListener('submit', (e) => {
     e.preventDefault();
     const id = e.target.elements[0].value;
@@ -78,6 +104,7 @@ const initApp = () => {
     });
   });
 
+  //Update function
   $edit.addEventListener('submit', (e) => {
     e.preventDefault();
     const id = e.target.elements[0].value;
@@ -91,6 +118,7 @@ const initApp = () => {
     });
   });
 
+  //Delete function
   $delete.addEventListener('submit', (e) => {
     e.preventDefault();
     const id = e.target.elements[0].value;
@@ -99,11 +127,12 @@ const initApp = () => {
       $deleteResult.innerHTML = `Deleted user ${id}`;
     })
     .catch(_e => {
-      $deleteResult.innerHTML = `Ooops... there was an error while trying to delete iser ${id}`;
+      $deleteResult.innerHTML = `Ooops... there was an error while trying to delete user ${id}`;
     });
   });
 };
 
+//wait for HTML page load and execute web3() ,initContract () functions  
 document.addEventListener('DOMContentLoaded', () => {
   initWeb3()
     .then(_web3 => {
